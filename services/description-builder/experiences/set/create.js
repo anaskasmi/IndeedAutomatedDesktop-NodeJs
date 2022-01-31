c7e6130a99e73990545315ec525f4e81c2f78926
Anas <anas@kasmi.dev>
2022-01-31 02:17:13 +0100

    fixed create set

--- services/description-builder/experiences/set/create.js
+++ services/description-builder/experiences/set/create.js
@@ -3,6 +3,6 @@ const ExperiencesSet = require("../../../../models/Experience/ExperiencesSet")
 exports.create = async(data) => {
     return ExperiencesSet.create({
         name: data.name,
-        experiences: []
+        experiences: data.experiences
     });
 }
