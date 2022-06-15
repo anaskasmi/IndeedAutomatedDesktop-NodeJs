4c1eaff8b86c8bd131795637d657207410071bb6
anaskasmi <anaskasmi98@gmail.com>
2022-06-15 17:17:08 +0100

    minor update

--- updater.js
+++ updater.js
@@ -2,7 +2,7 @@
 const shell = require('shelljs');
 
 const main = () => {
-    console.log('updater checking... ');
+    console.log('------- updater checking... -------');
     shell.exec(`npx kill-port 3009`);
     if (process.env.CURRENT_USER != "ANAS") {
         shell.exec(`git stash`);
@@ -10,7 +10,7 @@ const main = () => {
     shell.exec(`git pull origin master`);
     shell.exec(`npm i`);
 
-    console.log('updater done ');
+    console.log('------- updater done -------');
     shell.exec(`node app`);
 
 };
