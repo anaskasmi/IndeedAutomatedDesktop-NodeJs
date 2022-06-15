2cb412827c09972cd601cd477c09f38fac7281ee
anaskasmi <anaskasmi98@gmail.com>
2022-06-15 17:08:45 +0100

    new FE

--- updater.js
+++ updater.js
@@ -2,15 +2,16 @@
 const shell = require('shelljs');
 
 const main = () => {
-
-    console.log('updater running ');
-
+    console.log('updater checking... ');
     shell.exec(`npx kill-port 3009`);
     if (process.env.CURRENT_USER != "ANAS") {
         shell.exec(`git stash`);
     }
     shell.exec(`git pull origin master`);
     shell.exec(`npm i`);
+
+    console.log('updater done ');
     shell.exec(`node app`);
+
 };
 main();
