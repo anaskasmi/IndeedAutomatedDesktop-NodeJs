85bb1590935039dfa786d4d7965644d94bcf69f4
anaskasmi <anaskasmi98@gmail.com>
2022-06-15 17:06:49 +0100

    uncommented git stash in updater

--- updater.js
+++ updater.js
@@ -7,7 +7,7 @@ const main = () => {
 
     shell.exec(`npx kill-port 3009`);
     if (process.env.CURRENT_USER != "ANAS") {
-        // shell.exec(`git stash`);
+        shell.exec(`git stash`);
     }
     shell.exec(`git pull origin master`);
     shell.exec(`npm i`);
